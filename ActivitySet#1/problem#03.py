0ccc5e4101f12338599a9e922b5e6c3ed5eebd27
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-05-09 07:13:39 +0000

    done

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,8 @@
-# Variables, Expressions & Statements
+# Variables, Expressions & Statem
 
-hrs = float(input("Enter hours? "))
+hrs = input("Enter Hours:")
+rate= input("Enter Rate:")
+a=float(hrs)
+b=float(rate)
+gross= a*b
+print("Pay:",gross)
--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,2 @@
 # Conditional Execution
 
-hrs = input("Enter hours? ")
